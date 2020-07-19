ccace2594aab292b71909187872b02a0e6f763b1
oatmael <jaronharrison1605@gmail.com>
2020-07-19 23:16:18 +0930

    It's working! Big

--- express/app.js
+++ express/app.js
@@ -60,7 +60,13 @@ wsServer.on('request', function(request) {
             });
             break;
         case "revealTile":
-            revealTile(jsonMessage.x, jsonMessage.y);
+            revealTile(jsonMessage.x, jsonMessage.y, jsonMessage.uid);
+            break;
+        case "chat":
+            broadcast({
+                "message":jsonMessage.uid,
+                "value":jsonMessage.value
+            });
             break;
         default:
             broadcast({
@@ -76,66 +82,97 @@ wsServer.on('request', function(request) {
 });
 
 let lost = false;
-let board;
-let visibleBoard;
-function generateGameBoard(numMines, width, height){
-    board = new Array(Number(width)).fill(0).map(()=>new Array(Number(height)).fill(0));
-    visibleBoard = new Array(Number(width)).fill(-1).map(()=>new Array(Number(height)).fill(-1));
+let board = {
+    "width":0,
+    "height":0,
+    "mines":0,
+    "grid": []
+};
+let visibleBoard = {
+    "width":0,
+    "height":0,
+    "mines":0,
+    "grid": []
+};
 
-    //console.log(board);
+function generateGameBoard(numMines, width, height){
+    board.grid = new Array(Number(height)).fill(0).map(()=>new Array(Number(width)).fill(0));
+    visibleBoard.grid = new Array(Number(height)).fill(-1).map(()=>new Array(Number(width)).fill(-1));
+
+    board.width = Number(width);
+    visibleBoard.width = Number(width);
+    board.height = Number(height);
+    visibleBoard.height = Number(height);
+    board.mines = numMines;
+    visibleBoard.mines = numMines;
+    
 
     for (let i = 0; i < numMines; i++){
         let x = Math.floor(Math.random() * width);
         let y = Math.floor(Math.random() * height);
-        if (board[y][x] === 1){
+        if (board.grid[y][x] === 1){
             i--;
         } else {
-            board[y][x] = 1;
+            board.grid[y][x] = 1;
         }
     }
+
+    console.log(board);
 }
 generateGameBoard(20, 10, 10);
 
-function revealTile(x,y){
+function revealTile(x,y,uid){
     x = Number(x);
     y = Number(y);
     
-    if (board[y][x] === 1){
+    if (board.grid[y][x] === 1){
         lost = true;
         loseGame();
-    } else if (visibleBoard[y][x] === -1) {
-        numMines = 0;
-        for (let j = Math.max(y-1, 0); j <= Math.min(y+1, board.length); j++){
-            for (let i = Math.max(x-1, 0); i <= Math.min(x+1, board[0].length); i++){
-                if (board[j][i] === 1){
+    } else if (visibleBoard.grid[y][x] === -1) {
+        let numMines = 0;
+        for (let j = Math.max(y-1, 0); j <= Math.min(y+1, board.height - 1); j++){
+            for (let i = Math.max(x-1, 0); i <= Math.min(x+1, board.width - 1); i++){
+                console.log("Checking position: " + i + "," + j + " for mines")
+                if (board.grid[j][i] === 1){
                     numMines++;
                 } 
             }
         }
-        visibleBoard[y][x] = numMines;
-        console.log("tile revealed: " + x + "," + y + ": " + numMines);
+        console.log("Mines revealed: " + numMines);
+        visibleBoard.grid[y][x] = numMines;
+        //console.log("tile revealed: " + x + "," + y + ": " + numMines);
+        broadcast({
+            "message":"User " + uid + " Revealed Tile: ",
+            "value":x + "," + y
+        });
         broadcast({
             "message":"gameBoard",
             "value":visibleBoard
         });
-        if (numMines === 0) {
-            for (let i = Math.max(x-1, 0); i <= Math.min(x+1, board[0].length); i++){
-                for (let j = Math.max(y-1, 0); i <= Math.min(y+1, board[0].length); j++){
-                    revealTile(i,j);
+        if (visibleBoard.grid[y][x] === 0) {
+            for (let j = Math.max(y-1, 0); j <= Math.min(y+1, visibleBoard.height - 1); j++){
+                for (let i = Math.max(x-1, 0); i <= Math.min(x+1, visibleBoard.width - 1); i++){
+                    revealTile(i,j,uid);
                 }
             }
+        } else {
+            return;
         }
     }
 }
 
 function loseGame(){
-    for (let y = 0; y < board.length; y++){
-        for (let x = 0; x < board[0].length; x++){
-            if (board[x][y] === 1){
-                visibleBoard[x][y] = -2;
+    for (let y = 0; y < board.height; y++){
+        for (let x = 0; x < board.width; x++){
+            if (board.grid[y][x] === 1){
+                visibleBoard.grid[y][x] = -2;
             }
         }
     }
+    broadcast({
+        "message":"gameBoard",
+        "value":visibleBoard
+    });
 }
 
 function broadcast(jsonToSend) {
